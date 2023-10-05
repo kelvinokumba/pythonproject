036054842f0c75581d1c06ed1f1ef6a6a8f63694
Masibo <victor.wafula@student.moringaschool.com>
2023-10-05 11:01:03 +0300

    completed project

--- cli.py
+++ cli.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import click
 from datetime import datetime
 from sqlalchemy.orm import Session, sessionmaker
@@ -9,6 +11,19 @@ session = Session()
 
 class BookLibraryCLI:
     def add_book(self, title, author_id, genre_id, publication_date, isbn):
+        """
+        Add a new book to the library database.
+
+        Args:
+            title (str): The title of the book.
+            author_id (int): The ID of the author.
+            genre_id (int): The ID of the genre.
+            publication_date (str): The publication date in 'YYYY-MM-DD' format.
+            isbn (str): The ISBN of the book.
+
+        Returns:
+            None
+        """
         book = Book(
             title=title,
             author_id=author_id,
@@ -19,19 +34,37 @@ class BookLibraryCLI:
         session.add(book)
         session.commit()
 
-    def search_books(self, author=None, genre=None, book_id=None):
+    def search_books(self, author=None, genre=None):
+        """
+        Search for books in the library database based on author or genre.
+
+        Args:
+            author (str): The name of the author to search for.
+            genre (str): The genre to search for.
+
+        Returns:
+            list: A list of books that match the search criteria.
+        """
         query = session.query(Book)
         if author:
             query = query.filter(Book.author_name == author)
         elif genre:
             query = query.filter(Book.genre_name == genre)
-        elif book_id is not None:
-            query = query.filter(Book.id == book_id)
 
         books = query.all()
         return books
 
     def get_books(self, author=None, genre=None):
+        """
+        Get a list of books from the library database based on author or genre.
+
+        Args:
+            author (str): The name of the author to filter by.
+            genre (str): The genre to filter by.
+
+        Returns:
+            list: A list of books that match the filter criteria.
+        """
         query = session.query(Book)
 
         if author:
@@ -43,10 +76,18 @@ class BookLibraryCLI:
         return books
 
     def format_book_list(self, books):
+        """
+        Format a list of books into a user-friendly format.
+
+        Args:
+            books (list): A list of Book objects.
+
+        Returns:
+            list: A list of dictionaries representing formatted book information.
+        """
         formatted_books = []
         for book in books:
             formatted_book = {
-                'ID': book.id,
                 'Title': book.title,
                 'Author': book.author.name,
                 'Genre': ', '.join([genre.name for genre in book.genres]),
@@ -55,7 +96,25 @@ class BookLibraryCLI:
             }
             formatted_books.append(formatted_book)
         return formatted_books
+    
+    def delete_book(self, book_id):
+        """
+        Delete a book from the library database.
 
+        Args:
+            book_id (int): The ID of the book to delete.
+
+        Returns:
+            bool: True if the book was successfully deleted, False otherwise.
+        """
+        book = session.query(Book).filter_by(id=book_id).first()
+        if book:
+            session.delete(book)
+            session.commit()
+            return True
+        return False
+
+# Define CLI commands
 @click.command()
 @click.option('--title', prompt='Title')
 @click.option('--author_id', prompt='Author ID')
@@ -63,7 +122,7 @@ class BookLibraryCLI:
 @click.option('--publication_date', prompt='Publication Date (YYYY-MM-DD)')
 @click.option('--isbn', prompt='ISBN')
 def add_book_command(title, author_id, genre_id, publication_date, isbn):
-   
+    # Parse the publication_date string into a Python date object
     try:
         publication_date = datetime.strptime(publication_date, '%Y-%m-%d').date()
     except ValueError:
@@ -75,38 +134,75 @@ def add_book_command(title, author_id, genre_id, publication_date, isbn):
     click.echo("Book added successfully.")
 
 @click.command()
+@click.option('--book_id', prompt='Book ID to delete', type=int)
+def delete_book_command(book_id):
+    cli = BookLibraryCLI()
+    if cli.delete_book(book_id):
+        click.echo("Book deleted successfully.")
+    else:
+        click.echo("Book not found with the provided ID.")
+
+# Extend the search_command function to allow interaction
+@click.command()
 @click.option('--author', help='Search books by author')
 @click.option('--genre', help='Search books by genre')
-@click.option('--book_id', help='Search book by ID', type=int)
-def search_command(author, genre, book_id):
+@click.option('--author_id', help='Search books by author ID', type=int)
+def search_command(author, genre, author_id):
     cli = BookLibraryCLI()
-    if book_id is not None:
-        books = cli.search_books(book_id=book_id)
+
+    if author_id is not None:
+        books = cli.search_books(book_id=author_id)
     else:
-        books = cli.get_books(author, genre)
+        books = cli.get_books(author=author, genre=genre)
 
     if not books:
         click.echo("No books found.")
     else:
         formatted_books = cli.format_book_list(books)
         click.echo("Books found:")
-        for book in formatted_books:
-            for key, value in book.items():
-                click.echo(f"{key}: {value}")
-            click.echo("\n")
+        for index, book in enumerate(formatted_books, start=1):
+            click.echo(f"{index}. {book['Title']} by {book['Author']}")
+        
+        while True:
+            choice = click.prompt(
+                "Select a book to interact with (enter the number) or 'q' to quit: ",
+                type=str
+            )
 
+            if choice == 'q':
+                break
+            elif choice.isdigit():
+                index = int(choice) - 1
+                if 0 <= index < len(formatted_books):
+                    selected_book = formatted_books[index]
+                    click.echo(f"Selected Book: {selected_book['Title']} by {selected_book['Author']}")
+                    # Here, you can provide options for further interaction with the selected book
+                else:
+                    click.echo("Invalid book number. Please select a valid number.")
+            else:
+                click.echo("Invalid input. Enter a number or 'q' to quit.")
+
+# Define the welcome_message command
 @click.command()
 def welcome_message():
     click.echo("Welcome to the Book Library CLI!")
-    choice = click.prompt("Choose an option:\n1. Add a book\n2. Search for a book\n", type=int)
-    
-    if choice == 1:
-        add_book_command()
-    elif choice == 2:
-        book_id = click.prompt("Enter the Book ID to search", type=str)
-        search_command(book_id=book_id)
-    else:
-        click.echo("Invalid choice. Please select 1 or 2.")
+    while True:
+        choice = click.prompt(
+            "Choose an option:\n1. Add a book\n2. Search for a book\n3. Delete a book\n4. Quit\n", type=int
+        )
+        
+        if choice == 1:
+            add_book_command()
+        elif choice == 2:
+            search_command()
+        elif choice == 3:
+            delete_book_command()
+        elif choice == 4:
+            click.echo("Goodbye!")
+            break
+        else:
+            click.echo("Invalid choice. Please select 1, 2, 3, or 4.")
 
+# Main program execution
 if __name__ == '__main__':
     welcome_message()
--- models.py
+++ models.py
@@ -4,19 +4,25 @@ from sqlalchemy import Column, Integer, String, Date, ForeignKey, create_engine,
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
+# Create an SQLite database engine
 engine = create_engine('sqlite:///book_library.db')
 
+# Create a base class for declarative models
 Base = declarative_base()
 
+# Define a many-to-many association table between books and genres
 book_genre = Table(
     'book_genres',
-     Base.metadata,
-     Column('book_id', Integer, ForeignKey('books.id')),
-     Column('genre_id', Integer, ForeignKey('genres.id')),
-     extend_existing=True,
+    Base.metadata,
+    Column('book_id', Integer, ForeignKey('books.id')),
+    Column('genre_id', Integer, ForeignKey('genres.id')),
+    extend_existing=True,
 )
 
 class Book(Base):
+    """
+    Represents a book in the library.
+    """
     __tablename__ = 'books'
 
     id = Column(Integer, primary_key=True)
@@ -26,37 +32,50 @@ class Book(Base):
     publication_date = Column(Date)
     isbn = Column(String, unique=True)
 
-    author = relationship("Author", back_populates="books", foreign_keys=[author_id]) 
+    # Define a relationship with Author
+    author = relationship("Author", back_populates="books", foreign_keys=[author_id])
 
+    # Define a many-to-many relationship with Genre through the book_genre association table
     genres = relationship("Genre", secondary=book_genre, back_populates="books")
 
     def __repr__(self):
-        return f"Title: {self.title} , was published on {self.publication_date}"
+        return f"Title: {self.title}, was published on {self.publication_date}"
 
 class Author(Base):
+    """
+    Represents an author of a book.
+    """
     __tablename__ = 'authors'
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
     bio = Column(String)
 
+    # Define a one-to-many relationship with Book
     books = relationship('Book', back_populates='author', foreign_keys=[Book.author_id])
 
     def __repr__(self):
-        return f"Written by: {self.name}, description {self.bio}"
+        return f"Written by: {self.name}, description: {self.bio}"
 
 class Genre(Base):
+    """
+    Represents a genre of books.
+    """
     __tablename__ = 'genres'
 
     id = Column(Integer, primary_key=True)
     name = Column(String, nullable=False)
 
+    # Define a many-to-many relationship with Book through the book_genre association table
     books = relationship("Book", secondary=book_genre, back_populates="genres")
 
     def __repr__(self):
-        return f"Genre: {self.name}, is having an id of {self.id}"
+        return f"Genre: {self.name}, has an id of {self.id}"
 
 class UserProfile(Base):
+    """
+    Represents user profiles for authentication.
+    """
     __tablename__ = 'user_profiles'
 
     id = Column(Integer, primary_key=True)
@@ -76,4 +95,3 @@ if __name__ == "__main__":
     # Create a session to interact with the database
     Session = sessionmaker(bind=engine)
     session = Session()
-
